54e1f0ce95fdf3c441752748358acde223f7e02d
Daniel G <eritreawow@gmail.com>
2017-03-05 19:54:30 +0000

    Just practicing  at home 05-03-2017

---
+++ myApp.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Daniel on 05/03/2017.
+ */
+var app = angular.module('myApp1',[]);
+app.controller('firstCtrl',function($scope){
+    $scope.firstNum = 30;
+    $scope.secondNum = 12;
+
+});
