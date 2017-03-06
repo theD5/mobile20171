09089d88baf1a139011c05fb4712b0aa686544df
Daniel G <eritreawow@gmail.com>
2017-03-06 23:46:28 +0000

    lesson 3

--- myApp.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Daniel on 05/03/2017.
- */
-var app = angular.module('myApp1',[]);
-app.controller('firstCtrl',function($scope){
-    $scope.firstNum = 30;
-    $scope.secondNum = 12;
-
-});
